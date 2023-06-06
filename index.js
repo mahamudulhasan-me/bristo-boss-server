8648a0e1bb6a9d42daf326c5347f5eda31cdf0f4
Mahamdul Hasan <mahamudulhasan.org@gmail.com>
2023-06-06 13:44:32 +0600

    remove async befor mongodb client

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    client.connect();
 
     // create database
     const bristoBossDB = client.db("bristoBossDB");
